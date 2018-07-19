9df245d6cb65990663b4a242d7a7021428c51deb
martingaston <squidman@gmail.com>
2018-07-19 16:51:04 +0100

    Added basic 404 validation for pokeapi call - not linked up to Giphy yet. Relates #2 and #12

--- logic.js
+++ logic.js
@@ -4,61 +4,61 @@ function xhr(method, url, parsecb, domcb) {
     if (this.readyState == 4 && this.status == 200) {
       var result = JSON.parse(xhr.responseText);
       parsecb(result, domcb);
+    } else if (this.status == 404) {
+      parsecb(404, domcb);
     }
   };
   xhr.open(method, url, true);
   xhr.send();
 }
 
-// xhr("GET", "https://pokeapi.co/api/v2/pokemon/bulbasaur", pokeParse);
-//xhr("GET", "./bulbasaur.json", pokeParse);
-// xhr("GET", uri, parseCB, domCB)
-
-/*xhr(
-  "GET",
-  "https://api.giphy.com/v1/gifs/search?q=bulbasaur&limit=25&rating=pg-13&api_key=" +
-    giphyApiKey,
-  giphyParse
-); */
-
-
-
-
 function pokeDescripParse(input, domcb) {
   var pokeDescripResponse = input.flavor_text_entries;
-  
+
   for (var i = 0; i < pokeDescripResponse.length; i++) {
     if (pokeDescripResponse[i].language.name == "en") {
-       var pokeDescrip = pokeDescripResponse[i].flavor_text;
-       break;
+      var pokeDescrip = pokeDescripResponse[i].flavor_text;
+      break;
     }
   }
   domcb(pokeDescrip);
 
   // .flavor_text_entries
-	// .language.name == “en”
-	// . flavor_text
+  // .language.name == “en”
+  // . flavor_text
 }
 
 function pokeParse(data, domcb) {
-  typesParsed = [];
-  for (var i = 0; i < data.types.length; i++) {
-    typesParsed.push(data.types[i].type.name);
-  }
+  if (data == 404) {
+    var api404 = {
+      name: "Ditto (404)",
+      entryNumber: "404",
+      moves: ["Bad Spelling", "Silly Fingers", "Cat on Keyboard"],
+      type: ["Error"],
+      sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+    };
+    domcb(api404);
+  } else {
+    typesParsed = [];
+    for (var i = 0; i < data.types.length; i++) {
+      typesParsed.push(data.types[i].type.name);
+    }
 
-  movesParsed = [];
-  for (var i = 0; i < 3; i++) {
-    movesParsed.push(data.moves[i].move.name);
+    movesParsed = [];
+    for (var i = 0; i < 3; i++) {
+      movesParsed.push(data.moves[i].move.name);
+    }
+    var pokeApiResponse = {
+      name: data.name,
+      entryNumber: data.id,
+      moves: movesParsed,
+      type: typesParsed,
+      //description: data.description - STRETCHY GOAL
+      sprite: data.sprites.front_default
+    };
+    domcb(pokeApiResponse);
   }
-  var pokeApiResponse = {
-    name: data.name,
-    entryNumber: data.id,
-    moves: movesParsed,
-    type: typesParsed,
-    //description: data.description - STRETCHY GOAL
-    sprite: data.sprites.front_default
-  };
-  domcb(pokeApiResponse);
 }
 
 function giphyParse(input, domcb) {
@@ -67,8 +67,4 @@ function giphyParse(input, domcb) {
     parsedLinks.push(item.images.downsized.url);
   });
   domcb(parsedLinks);
-}
-
-if (typeof module !== "undefined") {
-  module.exports = pokeApiResponse;
 }
